3898756e3664e4acd94964b3243d21e3857e1144
Lauren Yu <laurenkyu@gmail.com>
2020-03-27 18:10:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// Add your Circle class here
+class Circle {
+  constructor(radius) {
+    this.radius = radius
+  }
+  
+  let pi = Math.PI
+  
+  get diameter() {
+    return this.radius * 2
+  }
+}
